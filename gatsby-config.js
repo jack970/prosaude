e0511cf428eb7914c8312746a878ec111c68a2bf
jack970 <italocod@hotmail.com>
2020-05-07 17:44:13 -0300

    add transformer csv

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
+      },
+    },
+    `gatsby-transformer-csv`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     {
