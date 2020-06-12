966d597fd069f12af249d01be955142892bbdf78
jack970 <italocod@hotmail.com>
2020-06-12 17:06:56 -0300

    update deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,7 +99,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: process.env.DEPLOY_URL ?
+        apiURL: process.env.DEPLOY_URL === 'production' ?
         'http://3.81.21.8:1337/' :"http://localhost:1337",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
